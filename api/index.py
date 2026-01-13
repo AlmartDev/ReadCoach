b865a50148d1de778e56925b50f1f50df780e315
almartdev <alonsomartinezcarratala@gmail.com>
2026-01-13 18:06:35 +0100

    Try fix for api

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if path not in sys.path:
+    sys.path.insert(0, path)
 
 from backend.app.main import app
 
-handler = app
+app = app
